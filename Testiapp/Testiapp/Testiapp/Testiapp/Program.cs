381f0559441402b54cdcd2072e3bb5e8c24a4305
Kiehuva <emme.nikkonen@gmail.com>
2022-01-10 12:24:02 +0200

    moi

---
+++ Testiapp/Testiapp/Testiapp/Testiapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testiapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
